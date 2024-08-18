bbaa0359fa7486222191c61f05bc59d0ee5ec91e
Stevan Popovic <stevan.popovic.headhog@gmail.com>
2024-08-18 20:45:02 +0200

    Added sound to main screen

--- Assets/StartMenuManager.cs
+++ Assets/StartMenuManager.cs
@@ -8,7 +8,7 @@ public class StartMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(GameObject.FindGameObjectWithTag("Music"));
     }
 
     // Update is called once per frame
